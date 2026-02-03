e5e84f182c7f7d19f7590d7802967944125aa0c8
Tian-Yizhou <yizhou.tian01@gmail.com>
2026-02-03 00:58:37 -0600

    add a new method to 2826

--- Problems/2826.sorting-three-groups.py
+++ Problems/2826.sorting-three-groups.py
@@ -1,7 +1,7 @@
 '''
 Author: Hannah
 Date: 2026-01-26 10:21:20
-LastEditTime: 2026-01-26 10:41:44
+LastEditTime: 2026-02-03 00:58:10
 '''
 #
 # @lc app=leetcode id=2826 lang=python3
@@ -15,17 +15,29 @@ class Solution:
 
         n = len(nums)
 
-        # equivalently, we can try to find the longest non-decreasing subsequence
-        # dp[i]: the length of longest non-decreasing subsequence of nums[:i]
-        dp = [0] * n
-
-        for i in range(n):
-            for j in range(i):
-                if nums[i] >= nums[j]:
-                    dp[i] = max(dp[j], dp[i])
-            
-            dp[i] += 1
+        # Method 2
+        # dp[x]: the length of longest non-decreasing subsequence ends with number x
+        dp = [0] * 4
+        for num in nums:
+            # for the current number, all the subsequence ends with x, where x <= num, 
+            # the length could add 1
+            dp[num] = max(dp[1:num+1]) + 1
         
         return n - max(dp)
+
+
+        # Method 1
+        # # equivalently, we can try to find the longest non-decreasing subsequence
+        # # dp[i]: the length of longest non-decreasing subsequence of nums[:i]
+        # dp = [0] * n
+
+        # for i in range(n):
+        #     for j in range(i):
+        #         if nums[i] >= nums[j]:
+        #             dp[i] = max(dp[j], dp[i])
+            
+        #     dp[i] += 1
+        
+        # return n - max(dp)
         # @lc code=end
 
