960dbb978c533eb5d4d7a03c79ae6b0832415302
Tian-Yizhou <yizhou.tian01@gmail.com>
2026-02-12 23:49:38 -0600

    1911, 2786 done

--- Solutions_Python/1911.maximum-alternating-subsequence-sum.py
+++ Solutions_Python/1911.maximum-alternating-subsequence-sum.py
@@ -1,3 +1,8 @@
+'''
+Author: Hannah
+Date: 2026-02-01 11:29:52
+LastEditTime: 2026-02-12 23:48:51
+'''
 #
 # @lc app=leetcode id=1911 lang=python3
 #
@@ -7,5 +12,47 @@
 # @lc code=start
 class Solution:
     def maxAlternatingSum(self, nums: List[int]) -> int:
+
+        n = len(nums)
+
+        # Method 2
+        # same logic as method 1
+        dp = [0, nums[0]]
+
+        for i in range(1, n):
+            pre_0 = dp[0]
+            dp[0] = max(pre_0, dp[1]-nums[i])
+            dp[1] = max(pre_0+nums[i], dp[1])
+
+        return dp[1]
+
+        # Method 1
+        # # dfs(i, 0): the max alternating sum of a subsequence of nums[:i+1],
+        # # which has even numbers
+        # # dfs(i, 1): the max alternating sum of subsequence of nums[:i+1],
+        # # which has odd numbers
+        # @cache
+        # def dfs(i, s):
+        #     # boundary, we only have nums[0]
+        #     if i == 0:
+        #         # the subsequence max AS has odd numbers is [nums[0]]
+        #         if s == 1:
+        #             return nums[0]
+        #         # the subsequence max AS has even numbers is []
+        #         else:
+        #             return 0
+        #     # if we find the subsequence of nums[:i+1] that has odd numbers
+        #     if s == 1:
+        #         # either use the subsequence of nums[:i] with even numbers and choose nums[i]
+        #         # or "inherit" a subsequence has odd elements that max AS from nums[:i-1]
+        #         return max(dfs(i-1, 0)+nums[i], dfs(i-1, 1))
+        #     # if we find the subsequence of nums[:i+1] that has even numbers
+        #     else:
+        #         # either use the subsequence of nums[:i] with odd numbers and choose nums[i]
+        #         # or "inherit" a subsequence has even elements that max AS from nums[:i-1]
+        #         return max(dfs(i-1, 1)-nums[i], dfs(i-1, 0))
+        
+        # return dfs(n-1, 1)
+
 # @lc code=end
 
--- Solutions_Python/2786.visit-array-positions-to-maximize-score.py
+++ Solutions_Python/2786.visit-array-positions-to-maximize-score.py
@@ -1,3 +1,8 @@
+'''
+Author: Hannah
+Date: 2026-02-01 11:24:15
+LastEditTime: 2026-02-12 23:16:42
+'''
 #
 # @lc app=leetcode id=2786 lang=python3
 #
@@ -7,6 +12,45 @@
 # @lc code=start
 class Solution:
     def maxScore(self, nums: List[int], x: int) -> int:
+
+        n = len(nums)
+
+        # Method 2
+        # dp[0]: the max score of nums[:j+1] ending with even number
+        # dp[1]: the max score of nums[:j+1] ending with odd number
+        # boundary: we cannot start from nowhere
+        dp = [-inf , -inf]
+        # initialization: we must start from nums[0]
+        if nums[0] % 2 == 1:
+            dp[1] = nums[0]
+        else:
+            dp[0] = nums[0]
+
+        for i in range(1, n):
+            if nums[i] % 2 == 0:
+                dp[0] = max(dp[0], dp[1]-x) + nums[i]
+            else:
+                dp[1] = max(dp[0]-x, dp[1]) + nums[i]
+        
+        return max(dp)
+    
+        # Method 1
+        # @cache
+        # def dfs(i, s):
+        #     # end if i exceed nums length
+        #     if i == len(nums):
+        #         return 0
+            
+        #     # if nums[i]'s parity is the same as s, we must choose it
+        #     # then for nums[i+1:], we either choose the same parity as s,
+        #     # or choose the different parity and pay x
+        #     if nums[i] % 2 == s:
+        #         return max(dfs(i+1, s), dfs(i+1, s^1)-x) + nums[i]
+        #     # if nums[i]'s parity is different from s, we skip it
+        #     else:
+        #         return dfs(i+1, s)
+            
+        # return dfs(0, nums[0]%2)
         
 # @lc code=end
 
