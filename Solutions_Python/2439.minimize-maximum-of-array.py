d86aa0ca41f4cb7f03a18bb63a8b994e6baef7bd
Tian-Yizhou <yizhou.tian01@gmail.com>
2026-02-15 00:45:41 -0600

    2439, 2517 done

--- Solutions_Python/2439.minimize-maximum-of-array.py
+++ Solutions_Python/2439.minimize-maximum-of-array.py
@@ -1,3 +1,8 @@
+'''
+Author: Hannah
+Date: 2026-02-01 11:26:05
+LastEditTime: 2026-02-14 23:14:50
+'''
 #
 # @lc app=leetcode id=2439 lang=python3
 #
@@ -7,6 +12,27 @@
 # @lc code=start
 class Solution:
     def minimizeArrayValue(self, nums: List[int]) -> int:
-        
+
+        # Method 2: for each nums[i], i > 0, 
+        # if nums[i] > avg(nums[:i]), then the avg(nums[:i+1]) is the min max bound
+        # if nums[i] < avg(nums[:i]), then the min max bound is the avg(nums[:i])
+        return max((s + i) // (i + 1) for i, s in enumerate(accumulate(nums)))
+
+
+        # Method 1
+        # given a limit, check whether all numbers in nums could <= limit
+        def check(limit: int) -> bool:
+            extra = 0
+            for i in range(len(nums) - 1, 0, -1):
+                # nums[i] take its own value and the part given by nums[i+1]
+                new_num = nums[i] + extra
+                extra = max(new_num - limit, 0)
+            return nums[0] + extra <= limit
+
+        # apply check to each element in range()
+        # find the first true in range(nums[0], max(nums))
+        # the range of minimum maximum integer is [nums[0], max(nums)]
+        return bisect_left(range(max(nums)), True, lo=nums[0], key=check)
+
 # @lc code=end
 
--- Solutions_Python/2517.maximum-tastiness-of-candy-basket.py
+++ Solutions_Python/2517.maximum-tastiness-of-candy-basket.py
@@ -1,3 +1,8 @@
+'''
+Author: Hannah
+Date: 2026-02-01 11:26:20
+LastEditTime: 2026-02-15 00:45:14
+'''
 #
 # @lc app=leetcode id=2517 lang=python3
 #
@@ -7,6 +12,69 @@
 # @lc code=start
 class Solution:
     def maximumTastiness(self, price: List[int], k: int) -> int:
+        price.sort()
+        n = len(price)
+
+        # Method 3
+        # we need to change function check because bisect takes a nondecreasing array
+        def check(t):
+            cnt = 1
+            pre = price[0]
+            for i in range(1, n):
+                if price[i]-pre >= t:
+                    cnt += 1
+                    pre = price[i]
+            
+            return False if cnt >= k else True
+        
+        # the search range of bisect
+        search_range = range((price[-1]-price[0]) // (k-1) + 1)
+        # find the last False then -1
+        # return bisect_right(search_range, False, key=check) - 1
+        # or find the first True then -1
+        return bisect_left(search_range, True, key=check) - 1
+
+        # check whether we can choose at least k candies if the tastiness is t
+        def check(t):
+            cnt = 1
+            pre = price[0]
+            for i in range(1, n):
+                if price[i]-pre >= t:
+                    cnt += 1
+                    pre = price[i]
+            
+            return True if cnt >= k else False
+
+        # Method 2: open set
+        # the range of possible tasiness
+        left = 0
+        # the min infeasible tasiness is the floor of (price[-1]-price[0])/(k-1)
+        right = (price[-1]-price[0]) // (k-1) + 1
+        # we want to find the last True
+        while left+1 < right:
+            mid = (left + right) // 2
+            if check(mid):
+                left = mid
+            else:
+                right = mid
+        
+        return left
+        
+        # Method 1
+        # the range of possible tasiness
+        # the lowest feasible tastiness is 0
+        left = 0
+        # the max feasible tasiness is the floor of (price[-1]-price[0])/(k-1)
+        right = (price[-1]-price[0]) // (k-1)
+        # we want to find the last True
+        while left <= right:
+            mid = (left + right) // 2
+            if check(mid):
+                left = mid + 1
+            else:
+                right = mid - 1
+        
+        return right
         
 # @lc code=end
 
