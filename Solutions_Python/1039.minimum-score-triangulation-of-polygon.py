ad1e0bfb8a831124279b7d158eddf5233f31e5ef
Tian-Yizhou <yizhou.tian01@gmail.com>
2026-03-23 21:00:05 -0500

    [DP] 516, 1039 done

--- Solutions_Python/1039.minimum-score-triangulation-of-polygon.py
+++ Solutions_Python/1039.minimum-score-triangulation-of-polygon.py
@@ -1,3 +1,8 @@
+'''
+Author: Hannah
+Date: 2026-02-01 11:31:38
+LastEditTime: 2026-03-23 20:59:29
+'''
 #
 # @lc app=leetcode id=1039 lang=python3
 #
@@ -5,8 +10,55 @@
 #
 
 # @lc code=start
+
+# from math import inf
+
 class Solution:
     def minScoreTriangulation(self, values: List[int]) -> int:
         
+        # Method: Dynamic Programming
+        # fix two vertices i, j, move (clockwise) a vertice k from i+1 to j-1
+        # for each k, vertices i to k consist a sub-polygon, 
+        # vertices k to j consist another sub-polygon
+        # so we need a function to find the min score of a polygon given two vertices
+
+        # Method 2: use array
+        n = len(values)
+        dp = [[0]*n for _ in range(n)]
+
+        # i starts from n-1 because j will take one vertice
+        for i in range(n-2, -1, -1):
+            # j is at least one vertice later than i
+            for j in range(i+1, n):
+                # if i starts from n-3, we can get rid of this judge
+                if i+1 == j:
+                    dp[i][j] = 0
+                else:
+                    score = inf
+                    for k in range(i+1, j):
+                        score = min(score, dp[i][k] + dp[k][j] + values[i]*values[k]*values[j])
+                    dp[i][j] = score
+        return dp[0][n-1]
+
+        # Method 1: use cache
+        # dp(i, j): return the min score of a polygon consist of vertices i to j
+        @cache
+        def dp(i, j):
+            # cannot construct a triangle
+            if i+1 == j:
+                return 0
+            # set initial answer
+            ans = inf
+            # for each vertice between i and j
+            for k in range(i+1, j):
+                # update answer
+                ans = min(ans, dp(i, k) + dp(k, j) + values[i]*values[j]*values[k])
+            
+            return ans
+        
+        n = len(values)
+
+        return dp(0, n-1)
+
 # @lc code=end
 
--- Solutions_Python/516.longest-palindromic-subsequence.py
+++ Solutions_Python/516.longest-palindromic-subsequence.py
@@ -1,3 +1,8 @@
+'''
+Author: Hannah
+Date: 2026-02-01 11:31:31
+LastEditTime: 2026-03-23 14:13:53
+'''
 #
 # @lc app=leetcode id=516 lang=python3
 #
@@ -5,8 +10,58 @@
 #
 
 # @lc code=start
+
+# from functools import cache
+
 class Solution:
     def longestPalindromeSubseq(self, s: str) -> int:
         
+        # Method: Dynamic Programming
+        # Time complexity: O(n^2)
+        # Space complexity: O(n^2)
+
+        # start from both ends, 
+        # if two char are the same, we should keep both, result add 2
+        # if two char are different, pick one that has longer subsequence length
+        
+        n = len(s)
+
+        # Method 2
+        dp = [[0]*n for _ in range(n)]
+        # when updating dp[i][j], we need updated dp[i+1][j-1], dp[i+1][j], dp[i][j-1]
+        # so we need to update reversely
+        for i in range(n-1, -1, -1):
+            # if sequence is empty, the value is 0 (initialization)
+            # if only one character, length is 1
+            dp[i][i] = 1
+            # when there is a sequence
+            for j in range(i+1, n):
+                if s[i] == s[j]:
+                    dp[i][j] = dp[i+1][j-1] + 2
+                else:
+                    dp[i][j] = max(dp[i+1][j], dp[i][j-1]) 
+        
+        return dp[0][n-1]
+
+        # Method 1
+        # dp(l, r): return the longest palindromic subsequence of s[l:r+1]
+        @cache
+        def dp(l, r):
+            # if string is empty, return 0
+            if l > r:
+                return 0
+            # if there is only one character, return 1
+            if l == r:
+                return 1
+            # if l < r
+            # if two characters are the same, keep both
+            if s[l] == s[r]:
+                return dp(l+1, r-1) + 2
+            # Otherwise, keep the char with longer subsequence
+            else:
+                return max(dp(l+1, r), dp(l, r-1))
+        
+        return dp(0, n-1)
+
 # @lc code=end
 
